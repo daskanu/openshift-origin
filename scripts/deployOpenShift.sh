bcfb06f63761d5d01a5c5f9f61afac6d04dda847
Josef Meier <jomeier@users.noreply.github.com>
2018-09-17 09:56:02 +0200

    Changed quotes in debug code.

--- scripts/deployOpenShift.sh
+++ scripts/deployOpenShift.sh
@@ -32,7 +32,7 @@ export STORAGEKIND=${24}
 
 echo "Start" >> ~/deployOpenshift.txt
 
-echo 'deployOpenShift.sh $1 "$2" $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21} ${22} ${23} ${24}' >> ~/deployOpenshift.sh
+echo "deployOpenShift.sh $1 $PASSWORD $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} $ACCOUNTKEY ${15} ${16} ${17} $AADCLIENTSECRET ${19} ${20} ${21} ${22} ${23} ${24}" >> ~/deployOpenshift.sh
 
 
 # Determine if Commercial Azure or Azure Government
